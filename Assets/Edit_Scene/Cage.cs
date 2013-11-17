efffc428f93bd33918740e24638a7d8cfc83c88d
Petro Korienev <soxjke@gmail.com>
2013-11-17 17:06:10 +0200

    Initial project commit

---
+++ Assets/Edit_Scene/Cage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cage : MonoBehaviour {
+	
+	public Material mat;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		
+	}
+	
+	void OnPostRender() 
+	{
+		GL.PushMatrix();
+		mat.SetPass(0);
+		GL.LoadIdentity();		
+		GL.Begin(GL.LINES);
+		GL.Color(Color.red);
+		GL.Vertex(new Vector3(0, 0, 10));
+		GL.Vertex(new Vector3(10, 10, 10));
+		GL.End();
+		GL.PopMatrix();
+		Debug.DrawLine(new Vector3(0, 0, 10), new Vector3(10, 10, 10), Color.red);
+	}
+}
---
+++ Assets/GUI_Scene/GUI_TEST.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_TEST : MonoBehaviour {
+	
+	private string textFieldString = "";
+	
+	void OnGUI()
+	{
+		float width = Screen.width;
+		float height = Screen.height;
+		GUI.Box(new Rect(2 * width / 3, 0, width / 3, height), "Properties");
+		textFieldString = GUI.TextField(new Rect(2 * width / 3, 10, width / 3 + 10, 30), textFieldString);
+	}
+	
+	void Start() 
+	{
+		TouchScreenKeyboard.hideInput = true;
+	}
+}
