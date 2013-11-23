f151b5a7422621188d5f300a2ba641b80f196647
Petro Korienev <soxjke@gmail.com>
2013-11-23 14:18:16 +0200

    Added fastJSON library

---
+++ Assets/Model/fastJSON/Getters.cs
@@ -0,0 +1,15 @@
+﻿#if !UNITY3D
+
+using System;
+using System.Collections.Generic;
+
+namespace fastJSON
+{
+    public sealed class DatasetSchema
+    {
+        public List<string> Info { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+#endif
---
+++ Assets/Model/fastJSON/Reflection.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Collections;
+
+namespace fastJSON
+{
+    internal struct Getters
+    {
+        public string Name;
+        public Reflection.GenericGetter Getter;
+        public Type propertyType;
+    }
+
+    internal sealed class Reflection
+    {
+        public readonly static Reflection Instance = new Reflection();
+        private Reflection()
+        {
+        }
+
+        public bool ShowReadOnlyProperties = false;
+        internal delegate object GenericSetter(object target, object value);
+        internal delegate object GenericGetter(object obj);
+        private delegate object CreateObject();
+
+        private SafeDictionary<Type, string> _tyname = new SafeDictionary<Type, string>();
+        private SafeDictionary<string, Type> _typecache = new SafeDictionary<string, Type>();
+#if !(UNITY3D && UNITY3D_IPHONE)
+        private SafeDictionary<Type, CreateObject> _constrcache = new SafeDictionary<Type, CreateObject>();
+#endif
+        private SafeDictionary<Type, List<Getters>> _getterscache = new SafeDictionary<Type, List<Getters>>();
+
+        #region [   PROPERTY GET SET   ]
+        internal string GetTypeAssemblyName(Type t)
+        {
+            string val = "";
+            if (_tyname.TryGetValue(t, out val))
+                return val;
+            else
+            {
+                string s = t.AssemblyQualifiedName;
+                _tyname.Add(t, s);
+                return s;
+            }
+        }
+
+        internal Type GetTypeFromCache(string typename)
+        {
+            Type val = null;
+            if (_typecache.TryGetValue(typename, out val))
+                return val;
+            else
+            {
+                Type t = Type.GetType(typename);
+                _typecache.Add(typename, t);
+                return t;
+            }
+        }
+
+        internal object FastCreateInstance(Type objtype)
+        {
+            try
+            {
+#if UNITY3D && UNITY3D_IPHONE
+				return Activator.CreateInstance(objtype);
+#else
+                CreateObject c = null;
+                if (_constrcache.TryGetValue(objtype, out c))
+                {
+                    return c();
+                }
+                else
+                {
+                    if (objtype.IsClass)
+                    {
+                        DynamicMethod dynMethod = new DynamicMethod("_", objtype, null);
+                        ILGenerator ilGen = dynMethod.GetILGenerator();
+                        ilGen.Emit(OpCodes.Newobj, objtype.GetConstructor(Type.EmptyTypes));
+                        ilGen.Emit(OpCodes.Ret);
+                        c = (CreateObject)dynMethod.CreateDelegate(typeof(CreateObject));
+                        _constrcache.Add(objtype, c);
+                    }
+                    else // structs
+                    {
+                        DynamicMethod dynMethod = new DynamicMethod("_", typeof(object), null);
+                        ILGenerator ilGen = dynMethod.GetILGenerator();
+                        var lv = ilGen.DeclareLocal(objtype);
+                        ilGen.Emit(OpCodes.Ldloca_S, lv);
+                        ilGen.Emit(OpCodes.Initobj, objtype);
+                        ilGen.Emit(OpCodes.Ldloc_0);
+                        ilGen.Emit(OpCodes.Box, objtype);
+                        ilGen.Emit(OpCodes.Ret);
+                        c = (CreateObject)dynMethod.CreateDelegate(typeof(CreateObject));
+                        _constrcache.Add(objtype, c);
+                    }
+                    return c();
+                }
+#endif
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(string.Format("Failed to fast create instance for type '{0}' from assemebly '{1}'",
+                    objtype.FullName, objtype.AssemblyQualifiedName), exc);
+            }
+        }
+		
+#if UNITY3D && UNITY3D_IPHONE
+		internal class ObjectDelegate {
+			
+			public readonly Type type;
+			public readonly FieldInfo field;
+			public readonly PropertyInfo property;
+			
+			private ObjectDelegate(Type type) {
+				this.type = type;
+			}
+
+			public ObjectDelegate(Type type, FieldInfo fieldInfor) : this(type) {
+				this.field = fieldInfor;
+			}
+			
+			public ObjectDelegate(Type type, PropertyInfo propertyInfor) : this(type) {
+				this.property = propertyInfor;
+			}
+			
+			public object Setter(object target, object value) {
+				lock(this) {
+					if (field != null && target != null) {
+						field.SetValue(target, value);
+					} else if (property != null && target != null) {
+						property.SetValue(target, value, null);
+					} else {
+						return target;
+					}
+					return target;
+				}
+			}
+			
+			public object Getter(object obj) {
+				lock (this) {
+					return field != null ? field.GetValue(obj) : (property != null ? property.GetValue(obj, null) : null);
+				}
+			}
+		}
+#endif
+
+        internal static GenericSetter CreateSetField(Type type, FieldInfo fieldInfo)
+        {
+#if UNITY3D && UNITY3D_IPHONE
+			return new GenericSetter(new ObjectDelegate(type, fieldInfo).Setter);
+#else
+            Type[] arguments = new Type[2];
+            arguments[0] = arguments[1] = typeof(object);
+
+            DynamicMethod dynamicSet = new DynamicMethod("_", typeof(object), arguments, type);
+
+            ILGenerator il = dynamicSet.GetILGenerator();
+
+            if (!type.IsClass) // structs
+            {
+                var lv = il.DeclareLocal(type);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Unbox_Any, type);
+                il.Emit(OpCodes.Stloc_0);
+                il.Emit(OpCodes.Ldloca_S, lv);
+                il.Emit(OpCodes.Ldarg_1);
+                if (fieldInfo.FieldType.IsClass)
+                    il.Emit(OpCodes.Castclass, fieldInfo.FieldType);
+                else
+                    il.Emit(OpCodes.Unbox_Any, fieldInfo.FieldType);
+                il.Emit(OpCodes.Stfld, fieldInfo);
+                il.Emit(OpCodes.Ldloc_0);
+                il.Emit(OpCodes.Box, type);
+                il.Emit(OpCodes.Ret);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldarg_1);
+                if (fieldInfo.FieldType.IsValueType)
+                    il.Emit(OpCodes.Unbox_Any, fieldInfo.FieldType);
+                il.Emit(OpCodes.Stfld, fieldInfo);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ret);
+            }
+            return (GenericSetter)dynamicSet.CreateDelegate(typeof(GenericSetter));
+#endif
+        }
+
+        internal static GenericSetter CreateSetMethod(Type type, PropertyInfo propertyInfo)
+        {
+#if UNITY3D && UNITY3D_IPHONE
+			return new GenericSetter(new ObjectDelegate(type, propertyInfo).Setter);
+#else
+            MethodInfo setMethod = propertyInfo.GetSetMethod();
+            if (setMethod == null)
+                return null;
+
+            Type[] arguments = new Type[2];
+            arguments[0] = arguments[1] = typeof(object);
+
+            DynamicMethod setter = new DynamicMethod("_", typeof(object), arguments);
+            ILGenerator il = setter.GetILGenerator();
+
+            if (!type.IsClass) // structs
+            {
+                var lv = il.DeclareLocal(type);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Unbox_Any, type);
+                il.Emit(OpCodes.Stloc_0);
+                il.Emit(OpCodes.Ldloca_S, lv);
+                il.Emit(OpCodes.Ldarg_1);
+                if (propertyInfo.PropertyType.IsClass)
+                    il.Emit(OpCodes.Castclass, propertyInfo.PropertyType);
+                else
+                    il.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
+                il.EmitCall(OpCodes.Call, setMethod, null);
+                il.Emit(OpCodes.Ldloc_0);
+                il.Emit(OpCodes.Box, type);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, propertyInfo.DeclaringType);
+                il.Emit(OpCodes.Ldarg_1);
+                if (propertyInfo.PropertyType.IsClass)
+                    il.Emit(OpCodes.Castclass, propertyInfo.PropertyType);
+                else
+                    il.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
+                il.EmitCall(OpCodes.Callvirt, setMethod, null);
+                il.Emit(OpCodes.Ldarg_0);
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (GenericSetter)setter.CreateDelegate(typeof(GenericSetter));
+#endif
+        }
+
+        internal static GenericGetter CreateGetField(Type type, FieldInfo fieldInfo)
+        {
+#if UNITY3D && UNITY3D_IPHONE
+			return new GenericGetter(new ObjectDelegate(type, fieldInfo).Getter);
+#else
+            DynamicMethod dynamicGet = new DynamicMethod("_", typeof(object), new Type[] { typeof(object) }, type);
+
+            ILGenerator il = dynamicGet.GetILGenerator();
+
+            if (!type.IsClass) // structs
+            {
+                var lv = il.DeclareLocal(type);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Unbox_Any, type);
+                il.Emit(OpCodes.Stloc_0);
+                il.Emit(OpCodes.Ldloca_S, lv);
+                il.Emit(OpCodes.Ldfld, fieldInfo);
+                if (fieldInfo.FieldType.IsValueType)
+                    il.Emit(OpCodes.Box, fieldInfo.FieldType);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldfld, fieldInfo);
+                if (fieldInfo.FieldType.IsValueType)
+                    il.Emit(OpCodes.Box, fieldInfo.FieldType);
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (GenericGetter)dynamicGet.CreateDelegate(typeof(GenericGetter));
+#endif
+        }
+
+        internal static GenericGetter CreateGetMethod(Type type, PropertyInfo propertyInfo)
+        {
+#if UNITY3D && UNITY3D_IPHONE
+			return new GenericGetter(new ObjectDelegate(type, propertyInfo).Getter);
+#else
+            MethodInfo getMethod = propertyInfo.GetGetMethod();
+            if (getMethod == null)
+                return null;
+
+            DynamicMethod getter = new DynamicMethod("_", typeof(object), new Type[] { typeof(object) }, type);
+
+            ILGenerator il = getter.GetILGenerator();
+
+            if (!type.IsClass) // structs
+            {
+                var lv = il.DeclareLocal(type);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Unbox_Any, type);
+                il.Emit(OpCodes.Stloc_0);
+                il.Emit(OpCodes.Ldloca_S, lv);
+                il.EmitCall(OpCodes.Call, getMethod, null);
+                if (propertyInfo.PropertyType.IsValueType)
+                    il.Emit(OpCodes.Box, propertyInfo.PropertyType);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, propertyInfo.DeclaringType);
+                il.EmitCall(OpCodes.Callvirt, getMethod, null);
+                if (propertyInfo.PropertyType.IsValueType)
+                    il.Emit(OpCodes.Box, propertyInfo.PropertyType);
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (GenericGetter)getter.CreateDelegate(typeof(GenericGetter));
+#endif
+        }
+
+        internal List<Getters> GetGetters(Type type)
+        {
+            List<Getters> val = null;
+            if (_getterscache.TryGetValue(type, out val))
+                return val;
+
+            PropertyInfo[] props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            List<Getters> getters = new List<Getters>();
+            foreach (PropertyInfo p in props)
+            {
+                if (!p.CanWrite && ShowReadOnlyProperties == false) continue;
+
+                object[] att = p.GetCustomAttributes(typeof(System.Xml.Serialization.XmlIgnoreAttribute), false);
+                if (att != null && att.Length > 0)
+                    continue;
+
+                GenericGetter g = CreateGetMethod(type, p);
+                if (g != null)
+                {
+                    Getters gg = new Getters();
+                    gg.Name = p.Name;
+                    gg.Getter = g;
+                    gg.propertyType = p.PropertyType;
+                    getters.Add(gg);
+                }
+            }
+
+            FieldInfo[] fi = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+            foreach (var f in fi)
+            {
+                object[] att = f.GetCustomAttributes(typeof(System.Xml.Serialization.XmlIgnoreAttribute), false);
+                if (att != null && att.Length > 0)
+                    continue;
+
+                GenericGetter g = CreateGetField(type, f);
+                if (g != null)
+                {
+                    Getters gg = new Getters();
+                    gg.Name = f.Name;
+                    gg.Getter = g;
+                    gg.propertyType = f.FieldType;
+                    getters.Add(gg);
+                }
+            }
+
+            _getterscache.Add(type, getters);
+            return getters;
+        }
+
+        #endregion
+    }
+}
